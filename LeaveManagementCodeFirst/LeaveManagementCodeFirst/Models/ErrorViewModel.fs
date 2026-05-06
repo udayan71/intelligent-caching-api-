7045e102edca96650d5946aa7eaeea416da7bc42
Udayan <udayantathe998@gmail.com>
2026-05-06 10:14:06 +0530

    Removed extra added projects.

--- LeaveManagementCodeFirst/LeaveManagementCodeFirst/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace LeaveManagementCodeFirst.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
--- studentcrudApp/studentcrudApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace studentcrudApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
